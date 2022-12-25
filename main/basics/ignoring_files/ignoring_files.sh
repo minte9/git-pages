f99ed84cf9ac7cb7abf2a2af46374a73853831d1
minte9 <catalinprescure@gmail.com>
2022-12-25 19:08:07 +0200

    Update repo

--- main/basics/ignoring_files/ignoring_files.sh
+++ main/basics/ignoring_files/ignoring_files.sh
@@ -9,17 +9,17 @@ alias gs="git status | sed 's/^/\t/'; echo"
 
 touch a.txt
 touch b.tmp
-echo "touch / Working tree:" ; gsp
+                                    ; echo "touch / Working tree:" ; gsp
 
-echo "*.tmp" > .gitignore # Look Here
-echo ".gitignore / Working tree:" ; gsp
+touch .gitignore                    ; echo "*.tmp" > .gitignore
+                                    ; echo ".gitignore / Working tree:" ; gsp
 
 git add .
-git commit -m 'm' > /dev/null ; echo "git commit / Repository:" ; gs
+git commit -m 'm' > /dev/null ;     ; echo "git commit / Repository:" ; gs
 
 git rm .gitignore > /dev/null
 git rm a.txt > /dev/null
-rm b.tmp ; echo "git rm / Repository:" ; gsp
+rm b.tmp                            ; echo "git rm / Repository:" ; gsp
 
 git add .
-git commit -m 'm' > /dev/null ; echo "git commit / Repository:" ; gs
+git commit -m 'm' > /dev/null       ; echo "git commit / Repository:" ; gs
