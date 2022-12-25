4c4f0f4fb24125ea83ae34eb6fcf6a16d367c176
minte9 <catalinprescure@gmail.com>
2022-12-25 18:51:00 +0200

    m

--- main/basics/states/states.sh
+++ main/basics/states/states.sh
@@ -15,20 +15,16 @@ alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
 alias gs="git status | sed 's/^/\t/'; echo"
 
 touch myfile.tmp
-    echo "touch / Working tree:" ; gsp
+    echo "touch / Working tree:"        ; gsp
 
 git add . # Look Here
-    echo "git add . / Staging area:"
-    gsp
+    echo "git add . / Staging area:"    ; gsp
 
 git commit -m "m" > /dev/null
-    echo "git commit / Repository:"
-    gs
+    echo "git commit / Repository:"     ; gs
 
 git rm myfile.tmp > /dev/null
-    echo "git rm / Staging area:"
-    gsp
+    echo "git rm / Staging area:"       ; gsp
 
 git commit -m "m" > /dev/null
-    echo "git commit / Repository:"
-    gs
+    echo "git commit / Repository:"     ; gs
