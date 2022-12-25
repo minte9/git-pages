96408c8c13ef5bbeb2d1b416da8813373fdd8e5f
minte9 <catalinprescure@gmail.com>
2022-12-25 20:15:28 +0200

    Update repo

--- main/basics/removing_files/removing_files.sh
+++ main/basics/removing_files/removing_files.sh
@@ -11,7 +11,7 @@ Reset unstages a staged file
 alias gsp="git status --porcelain | sed 's/^/\t/'"
 alias gs="git status | sed 's/^/\t/'"
 
-touch a.txt                   ; echo 'touch'  ; git status --porcelain
-git add a.txt                 ; echo 'git add'  ; git status --porcelain
-git rm --cached a.txt         ; echo 'git rm --cached' ; git status --porcelain 
+touch a.txt                   ; echo 'touch'  ; gsp
+git add a.txt                 ; echo 'git add'  ; gsp
+git rm --cached a.txt > /dev/null        ; echo 'git rm --cached' ; gsp 
 #git rm a.txt > /dev/null        ; git status --porcelain
