ee879efab85278b3107049c86854839605927775
minte9 <catalinprescure@gmail.com>
2022-12-25 18:58:33 +0200

    m

--- main/basics/states/states.sh
+++ main/basics/states/states.sh
@@ -14,10 +14,8 @@ Committed file is stored in your DB
 alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
 alias gs="git status | sed 's/^/\t/'; echo"
 
-touch myfile.tmp ;              echo "touch / Working tree:"        ; gsp
-
-git add . ;                     echo "git add . / Staging area:"    ; gsp
-git commit -m "m" > /dev/null ; echo "git commit / Repository:"     ; gs
-
-git rm myfile.tmp > /dev/null ; echo "git rm / Staging area:"       ; gsp
-git commit -m "m" > /dev/null ; echo "git commit / Repository:"     ; gs
+touch myfile.tmp                ; echo "touch / Working tree:"        ; gsp
+git add .                       ; echo "git add . / Staging area:"    ; gsp
+git commit -m "m" > /dev/null   ; echo "git commit / Repository:"     ; gs
+git rm myfile.tmp > /dev/null   ; echo "git rm / Staging area:"       ; gsp
+git commit -m "m" > /dev/null   ; echo "git commit / Repository:"     ; gs
