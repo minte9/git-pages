3be0c4603010d14dc596099932a3bd7c41554d03
minte9 <catalinprescure@gmail.com>
2022-12-25 20:36:00 +0200

    Update repo

--- main/basics/removing_files/removing_files.sh
+++ main/basics/removing_files/removing_files.sh
@@ -17,6 +17,3 @@ git rm --cached a.tmp > /dev/null   ; echo 'git rm --cached' ; gsp
 git add a.tmp                       ; echo 'git add'         ; gsp
 git rm -f a.tmp > /dev/null         ; echo 'git rm -f'       ; gs
 
-touch a.tmp                         ; echo 'touch'           ; gsp
-git add a.tmp                       ; echo 'git add'
-git reset HEAD -- a.tmp             ; echo 'git reset'       ; gs
