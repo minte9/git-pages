cd3c454452897b396e54bdc4888ce2fc0d7838ba
minte9 <catalinprescure@gmail.com>
2022-12-25 20:25:29 +0200

    Update repo

--- main/basics/removing_files/removing_files.sh
+++ main/basics/removing_files/removing_files.sh
@@ -5,14 +5,16 @@
 If a file is not tracked rm file will generate error
 To remove a file and keep it in working directory, use --cached
 To remove a file from working directory, when used with -f
-Reset unstages a staged file
+Alternatively, you can use reset to untrack files
 "
 
 alias gsp="git status --porcelain | sed 's/^/\t/' ; echo"
 alias gs="git status | sed 's/^/\t/' ; echo"
 
-touch a.tmp                        ; echo 'touch'           ; gsp
-git add a.tmp                      ; echo 'git add'         ; gsp
-git rm --cached a.tmp > /dev/null  ; echo 'git rm --cached' ; gsp 
-git add a.tmp                      ; echo 'git add'         ; gsp
-git rm -f a.tmp > /dev/null        ; echo 'git rm -f'       ; gs
+touch a.tmp                         ; echo 'touch'           ; gsp
+git add a.tmp                       ; echo 'git add'         ; gsp
+git rm --cached a.tmp > /dev/null   ; echo 'git rm --cached' ; gsp 
+git add a.tmp                       ; echo 'git add'         ; gsp
+git rm -f a.tmp > /dev/null         ; echo 'git rm -f'       ; gs
+git add a.tmp                       ; echo 'git add'         ; gsp
+git reset HEAD -- a.tmp             ; echo 'git reset'       ; gs
