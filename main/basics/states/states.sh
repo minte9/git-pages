6ca24744f85bc86b0511c42536c528f9b01ceca6
minte9 <catalinprescure@gmail.com>
2022-12-25 19:02:51 +0200

    git-pages update

--- main/basics/states/states.sh
+++ main/basics/states/states.sh
@@ -3,9 +3,9 @@
 : """
 Git has three main states that your files can reside in:
 
-Working tree / File changed but not committed it to database yet
-Staging area / File marked as modified to go into your next commit
-Repository / Committed file is stored in your DB
+Working tree    | File changed but not committed it to database yet
+Staging area    | File marked as modified to go into your next commit
+Repository      | Committed file is stored in your DB
 
 The history of the project is on the local disk
 GIT stores everything in his database using hashes
