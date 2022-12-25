fb137d2176afb0ea09a8d78de9d740421e3eaa04
minte9 <catalinprescure@gmail.com>
2022-12-25 20:04:36 +0200

    Update repo

---
+++ main/basics/removing_files/removing_files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+: "Removing files from repository
+
+If a file is not tracked rm file will generate error
+To remove a file from working directory, when used with -f
+To keep the file on the working directory, use --cached
+Reset unstages a staged file
+"
+
+alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
+alias gs="git status | sed 's/^/\t/'; echo"
+
+touch a.txt ; gsp
+git add a.txt ; gsp
+git rm -f myfile ; gsp
