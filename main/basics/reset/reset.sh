b637437a2e6cd84ea52ec959c48c8f70dfb8cc7e
minte9 <catalinprescure@gmail.com>
2022-12-26 14:13:49 +0200

    output.sh - update repo

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -11,8 +11,10 @@ git add .       ; echo "git add / Staging area"  ; gsp
 git commit -m 'First commit' | ltab
                   echo "git log / Show first:"
 git log -n 1 --oneline | ltab
-                  echo "Reset soft / Staging area:"
+                  echo "Reset soft ~1/ Staging area:"
 git reset --soft HEAD~1 ; gsp
+                  echo "Reset ~1/ Staging area:"
+git reset HEAD~1 ; gsp
                   echo "Repo clean up"
 git commit -m 'm' --quiet
 git rm a.txt > /dev/null
