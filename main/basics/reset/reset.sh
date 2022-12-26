810da88f57cb37471ebc3c6e1d58a74a9ac9ccc6
minte9 <catalinprescure@gmail.com>
2022-12-26 14:07:10 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -7,15 +7,12 @@
 
 touch a.txt     ; echo "touch / Working tree;"    ; gsp
 git add .       ; echo "git add / Staging area"  ; gsp
-
-echo "git commit /"
-git commit -m 'First commit' | sed 's/^/\t/' ; echo
-
-echo "git log / Show first:"
-git log -n 1 --oneline | sed 's/^/\t/' ; echo
-
-echo "Reset soft / Staging area:"
+                  echo "git commit /"
+git commit -m 'First commit' | ltab
+                  echo "git log / Show first:"
+git log -n 1 --oneline | ltab
+                  echo "Reset soft / Staging area:"
 git reset --soft HEAD~1 ; gsp
-
+                  echo "Repo clean up"
 git commit -m 'm' --quiet
 git rm a.txt > /dev/null
