04e22f623e7e809f2eb624f3955fdb1bf586e56d
minte9 <catalinprescure@gmail.com>
2022-12-26 13:59:44 +0200

    Commit reset

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -7,16 +7,13 @@
 
 touch a.txt     ; echo "touch /"    ; gsp
 git add .       ; echo "git add /"  ; gsp
-git commit -m 'First commit'
+git commit -m 'First commit'; echo
 
 echo "Log"
-git log -n 2 --oneline
+git log -n 2 --oneline : echo
 
 echo "Reset"
 git reset --soft HEAD~1 ; gsp
-
-echo "Log"
-git log -n 2 --oneline
 
 git commit -m 'Commit reset' --quiet
 git rm a.txt > /dev/null
