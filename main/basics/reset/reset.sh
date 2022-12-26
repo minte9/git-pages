063aacf097524a6522deaa6030765df2dcd59f4f
minte9 <catalinprescure@gmail.com>
2022-12-26 13:27:45 +0200

    first

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -7,7 +7,7 @@
 
 touch a.txt     ; echo "touch / Working tree:"      ; gsp
 git add .
-git commit -m 'first' > /dev/null
+git commit -m 'first'
 
 touch b.txt     ; echo "touch / Working tree:"      ; gsp
 git add .
