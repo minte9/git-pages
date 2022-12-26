47d52f734d8b8b9033b5c7c4fdde8a1bf8064610
minte9 <catalinprescure@gmail.com>
2022-12-26 15:02:31 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -14,6 +14,6 @@ echo "git add"          ; git add .                     ; gsp
 echo "git commit"       ; git commit -m 'First commit'  | tab2lines
 echo "git last log"     ; git log -n 1 --oneline        | tab2lines 
 echo "Reset soft ~1"    ; git reset --soft HEAD~1       ; gsp ; gc
-echo "Reset ~1"         ; git reset HEAD~1              ; gsp; gc
+echo "Reset ~1"         ; git reset HEAD~1              ; gsp ; gc
                   
 git rm -f a.txt > /dev/null
