8e0630731a2fcb197be6885f9dec8936d50a19d3
minte9 <catalinprescure@gmail.com>
2022-12-26 14:36:48 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -14,6 +14,6 @@ git log -n 1 --oneline | tab2lines
                   echo "Reset soft ~1"
 git reset --soft HEAD~1 ; gsp ; gacq
                   echo "Reset ~1"
-git reset HEAD~1 ; gsp ; gcq
+git reset HEAD~1 ; gsp ; gacq
                   
 git rm -f a.txt > /dev/null
