72527c7c0ba6f360bfdffeb6c000289b690df60d
minte9 <catalinprescure@gmail.com>
2022-12-26 14:55:39 +0200

    git-pages update

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-: "
+: "Commit reset
+Reset soft - modifications remain in staging
+Reset normal - modifications are removed from staging
+Reset hard - modifications are removed, but remains in gargage
+You can get the commit back as long as it was within a few days
 "
 
 . "../../aliases.sh"
