de39b42220e760abc972c9baf07f88d813df6a72
minte9 <catalinprescure@gmail.com>
2022-12-26 13:41:49 +0200

    First commit

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -5,7 +5,7 @@
 
 . "../../aliases.sh"
 
-touch a.txt     ; echo "touch / "      ; git status --porcelain
+touch a.txt     ; echo "touch / "      ; gsp
 git add .      ; echo "git add / " ; gsp
 git commit -m 'First commit' --quiet
 git log -n 1 --oneline
