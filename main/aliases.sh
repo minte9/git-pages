dd4cd4f767d2045653b58a0abae7b9cb4e77f3f7
minte9 <catalinprescure@gmail.com>
2022-12-26 14:36:48 +0200

    output.sh - update repo

--- main/aliases.sh
+++ main/aliases.sh
@@ -1,5 +1,4 @@
 alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
 alias gs="git status | sed 's/^/\t/'; echo"
 alias tab2lines="sed 's/^/\t/' ; echo"
-alias gcq="git commit -m 'm' --quiet"
 alias gacq="git add . ; git commit -m 'm' --quiet"
