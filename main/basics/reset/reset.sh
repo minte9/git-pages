b316701767878ccfaa30c219974877c59dd4742b
minte9 <catalinprescure@gmail.com>
2022-12-26 14:28:26 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -5,15 +5,15 @@
 
 . "../../aliases.sh"
 
-touch a.txt     ; echo "touch:"    ; gsp
-git add .       ; echo "git add:"  ; gsp
+touch a.txt     ; echo "touch"    ; gsp
+git add .       ; echo "git add"  ; gsp
                   echo "git commit"
 git commit -m 'First commit' | tab2lines
-                  echo "git last log:"
+                  echo "git last log"
 git log -n 1 --oneline | tab2lines
-                  echo "Reset soft ~1:"
-git reset --soft HEAD~1 ; gsp
-                  echo "Reset ~1" ; gcq
-git reset HEAD~1 ; gsp
-                  echo "Repo clean up" ; gcq
+                  echo "Reset soft ~1"
+git reset --soft HEAD~1 ; gsp ; gcq
+                  echo "Reset ~1"
+git reset HEAD~1 ; gsp ; gcq
+                  
 git rm a.txt > /dev/null
