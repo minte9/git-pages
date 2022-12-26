b9f27118bbcfcb44529d4a7e8386295e3c6cae2e
minte9 <catalinprescure@gmail.com>
2022-12-26 14:15:43 +0200

    output.sh - update repo

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -14,6 +14,7 @@ git log -n 1 --oneline | ltab
                   echo "Reset soft ~1/ Staging area:"
 git reset --soft HEAD~1 ; gsp
                   echo "Reset ~1/ Staging area:"
+git commit -m 'm' --quiet
 git reset HEAD~1 ; gsp
                   echo "Repo clean up"
 git commit -m 'm' --quiet
