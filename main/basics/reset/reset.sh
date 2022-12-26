79fbe1ab700ca628b05790e4ccb420683d3055bd
minte9 <catalinprescure@gmail.com>
2022-12-26 13:42:31 +0200

    First commit

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -8,14 +8,8 @@
 touch a.txt     ; echo "touch / "      ; gsp
 git add .      ; echo "git add / " ; gsp
 git commit -m 'First commit' --quiet
-git log -n 1 --oneline
-
-touch b.txt     ; echo "touch / Working tree:"      ; gsp
-git add .
-git commit -m 'second' > /dev/null       ; echo "git commit / Repository:"   ; gs
 
 echo "git log /"
 git log -n 2 --oneline
 
 git rm a.txt > /dev/null
-git rm b.txt > /dev/null
