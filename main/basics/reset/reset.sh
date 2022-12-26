2a9744d5a8897131605b4e26926fa82d5d9ce5d9
minte9 <catalinprescure@gmail.com>
2022-12-26 14:25:52 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -5,15 +5,15 @@
 
 . "../../aliases.sh"
 
-touch a.txt     ; echo "touch / Working tree;"    ; gsp
-git add .       ; echo "git add / Staging area"  ; gsp
+touch a.txt     ; echo "touch / Status:"    ; gsp
+git add .       ; echo "git add / Status:"  ; gsp
                   echo "git commit /"
 git commit -m 'First commit' | tab2lines
-                  echo "git log / Show last log:"
+                  echo "git log / Last:"
 git log -n 1 --oneline | tab2lines
-                  echo "Reset soft ~1 / Staging area:"
+                  echo "Reset soft ~1 / Status:"
 git reset --soft HEAD~1 ; gsp
-                  echo "Reset ~1 / Working:" ; gcq
+                  echo "Reset ~1 / Status:" ; gcq
 git reset HEAD~1 ; gsp
                   echo "Repo clean up" ; gcq
 git rm a.txt > /dev/null
