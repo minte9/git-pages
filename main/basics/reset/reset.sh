4d7fcbcf74de99b2dbf6ead01eeffbe658eafce9
minte9 <catalinprescure@gmail.com>
2022-12-26 14:26:38 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -5,15 +5,15 @@
 
 . "../../aliases.sh"
 
-touch a.txt     ; echo "touch / Status:"    ; gsp
-git add .       ; echo "git add / Status:"  ; gsp
-                  echo "git commit /"
+touch a.txt     ; echo "touch:"    ; gsp
+git add .       ; echo "git add:"  ; gsp
+                  echo "git commit"
 git commit -m 'First commit' | tab2lines
-                  echo "git log / Last:"
+                  echo "git last log:"
 git log -n 1 --oneline | tab2lines
-                  echo "Reset soft ~1 / Status:"
+                  echo "Reset soft ~1:"
 git reset --soft HEAD~1 ; gsp
-                  echo "Reset ~1 / Status:" ; gcq
+                  echo "Reset ~1" ; gcq
 git reset HEAD~1 ; gsp
                   echo "Repo clean up" ; gcq
 git rm a.txt > /dev/null
