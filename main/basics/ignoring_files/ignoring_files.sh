534d0bd809d02cdb327ae75e61972986f17a75f8
minte9 <catalinprescure@gmail.com>
2022-12-26 11:54:25 +0200

    m

--- main/basics/ignoring_files/ignoring_files.sh
+++ main/basics/ignoring_files/ignoring_files.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 : "Create a file .gitignore containing a list of patterns to match
+Use . (source command) to exectute aliasses.sh in the current shell
 "
 
 . ../../aliases.sh
@@ -8,8 +9,10 @@
 touch a.txt b.tmp                   ; echo "touch / Working tree:"      ; gsp
 touch .gitignore
 echo "*.tmp" > .gitignore           ; echo ".gitignore / Working tree:" ; gsp
+
 git add .
 git commit -m 'm' > /dev/null       ; echo "git commit / Repository:"   ; gs
+
 rm b.tmp 
 git rm .gitignore a.txt > /dev/null ; echo "git rm / Repository:"       ; gsp
 git add .
