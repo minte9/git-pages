863eef280f47191b7e2b3922887fa2ed6a37874f
minte9 <catalinprescure@gmail.com>
2022-12-26 14:03:16 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -5,14 +5,14 @@
 
 . "../../aliases.sh"
 
-touch a.txt     ; echo "touch /"    ; gsp
-git add .       ; echo "git add /"  ; gsp
+touch a.txt     ; echo "touch / Working tree;"    ; gsp
+git add .       ; echo "git add / Staging area"  ; gsp
 git commit -m 'First commit'; echo
 
-echo "Log"
+echo "git log / Show first:"
 git log -n 1 --oneline ; echo
 
-echo "Reset"
+echo "Reset soft / Staging area:"
 git reset --soft HEAD~1 ; gsp
 
 git commit -m 'm' --quiet
