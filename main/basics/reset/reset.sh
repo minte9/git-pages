c5fb7d12bcee792bd89879fe95d47cb7c7b64ec6
minte9 <catalinprescure@gmail.com>
2022-12-26 13:59:58 +0200

    Commit reset

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -10,7 +10,7 @@ git add .       ; echo "git add /"  ; gsp
 git commit -m 'First commit'; echo
 
 echo "Log"
-git log -n 2 --oneline : echo
+git log -n 2 --oneline ; echo
 
 echo "Reset"
 git reset --soft HEAD~1 ; gsp
