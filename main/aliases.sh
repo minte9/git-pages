ddaf4a87c99985db4e6461539157c222e2984070
minte9 <catalinprescure@gmail.com>
2022-12-26 14:23:23 +0200

    output.sh - update repo

--- main/aliases.sh
+++ main/aliases.sh
@@ -1,3 +1,4 @@
 alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
 alias gs="git status | sed 's/^/\t/'; echo"
-alias ltab="sed 's/^/\t/' ; echo"
+alias tab2lines="sed 's/^/\t/' ; echo"
+alias gcq="git commit -am 'm' --quiet"
--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -8,14 +8,12 @@
 touch a.txt     ; echo "touch / Working tree;"    ; gsp
 git add .       ; echo "git add / Staging area"  ; gsp
                   echo "git commit /"
-git commit -m 'First commit' | ltab
-                  echo "git log / Show first:"
-git log -n 1 --oneline | ltab
-                  echo "Reset soft ~1/ Staging area:"
+git commit -m 'First commit' | tab2lines
+                  echo "git log / Show last log:"
+git log -n 1 --oneline | tab2lines
+                  echo "Reset soft ~1 / Staging area:"
 git reset --soft HEAD~1 ; gsp
-                  echo "Reset ~1/ Staging area:"
-git commit -am 'm' --quiet
+                  echo "Reset ~1 / Working:" ; gcq
 git reset HEAD~1 ; gsp
-                  echo "Repo clean up"
-git commit -am 'm' --quiet
+                  echo "Repo clean up" : gcq
 git rm a.txt > /dev/null
