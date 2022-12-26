fd1a8a22bd58ad2ba5a9f7c7d976abcf3909a891
minte9 <catalinprescure@gmail.com>
2022-12-26 15:17:38 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -11,7 +11,7 @@ You can get the commit back as long as it was within a few days
 
 echo "touch"            ; touch a.txt                   ; gsp
 echo "git add"          ; git add .                     ; gsp
-echo "git commit"       ; git commit -m 'First commit'  | tab2lines
+echo "git commit"       ; git commit -m 'My commit'     | tab2lines
 echo "git last log"     ; git log -n 1 --oneline        | tab2lines 
 echo "Reset soft ~1"    ; git reset --soft HEAD~1       ; gsp ; gc
 echo "Reset ~1"         ; git reset HEAD~1              ; gsp ; gc
