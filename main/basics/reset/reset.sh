530114b38ac3ff7a89666808186bf7c9b8714f74
minte9 <catalinprescure@gmail.com>
2022-12-26 14:59:39 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -9,15 +9,12 @@ You can get the commit back as long as it was within a few days
 
 . "../../aliases.sh"
 
-touch a.txt     ; echo "touch"    ; gsp
-git add .       ; echo "git add"  ; gsp
-                  echo "git commit"
-git commit -m 'First commit' | tab2lines
-                  echo "git last log"
-git log -n 1 --oneline | tab2lines
-                  echo "Reset soft ~1"
-git reset --soft HEAD~1 ; gsp ; gc
-                  echo "Reset ~1"
-git reset HEAD~1 ; gsp; gc
+touch a.txt             ; echo "touch"                  ; gsp
+git add .               ; echo "git add"                ; gsp
+
+echo "git commit"       ; git commit -m 'First commit'  | tab2lines
+echo "git last log"     ; git log -n 1 --oneline        | tab2lines 
+echo "Reset soft ~1"    ; git reset --soft HEAD~1       ; gsp ; gc
+echo "Reset ~1"         ; git reset HEAD~1              ; gsp; gc
                   
-git rm -f a.txt > /dev/null # Repo clean up
+git rm -f a.txt > /dev/null # clean up
