16d7ab3d6b61ce4db5a844b86c6c9cc8308a85d4
minte9 <catalinprescure@gmail.com>
2022-12-27 13:17:52 +0200

    output.sh - update repo

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-: "Difference beetwen file modficiation and its last commit
+: "Difference beetwen file modficiations and its last commit
 "
 
 . "../../aliases.sh"
 
 echo "touch & write"        ; touch myfile.tmp     
 echo "A" > myfile.tmp
-echo "git add"              ; git add . ; git commit -am 'm'
+echo "git add"              ; git add . ; gsp
 
 echo "overwrite"
 echo "B" > "myfile.tmp"
 echo "C" >> "myfile.tmp"
 echo "git diff"             ; git diff HEAD^  "myfile.tmp"   | tab2lines
 
-git rm -f "myfile.tmp" > /dev/null
+#git rm -f "myfile.tmp" > /dev/null
