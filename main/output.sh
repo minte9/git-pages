bd8517d0dc2202892f36f58545e9e230e1b29f54
minte9 <catalinprescure@gmail.com>
2022-12-27 12:10:19 +0200

    output.sh - update repo

--- main/output.sh
+++ main/output.sh
@@ -7,6 +7,12 @@ cd git-pages/main/basics/states
 ../../output.sh states.sh 
 
 $1 - first command argument
+
+Use basename to extract name from $1 argument filename
+The cut command is used to split the file name on the . character
+-f1 option specifies that the first field 
+-d. option specifies that the . character should be used as the delimiter
+
 "$output" - command substitution
 tee - outputs & save
 """
@@ -15,8 +21,8 @@ filename=$(basename "$1" | cut -f1 -d.)
 echo "$filename"
 
 output=`./$1`
-echo "$output" | tee output.log 
+echo "$output" | tee "$filename"".log" 
 
-git add output.log
+git add .
 git add ../../
 git commit -am 'output.sh - update repo' > /dev/null
