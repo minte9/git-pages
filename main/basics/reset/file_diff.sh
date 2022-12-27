a67330586f2c7b1c9a5fc9cd970243a69e3d970d
minte9 <catalinprescure@gmail.com>
2022-12-27 12:23:03 +0200

    My commit

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -5,13 +5,13 @@
 
 . "../../aliases.sh"
 
-echo "1" > "myfile.tmp"
+echo -n "1" > "myfile.tmp"
 
 echo "git add"          ; git add .                     ; gsp
 echo "git commit"       ; git commit -m 'My commit'     | tab2lines
 echo "git last log"     ; git log -n 1 --oneline        | tab2lines 
 
-echo -n "2" >> "myfile.tmp"
+echo "2" >> "myfile.tmp"
 git diff HEAD^ "myfile.tmp"
                   
 git rm -f "myfile.tmp" > /dev/null
