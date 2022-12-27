588a5971c3b3c59c70a8d12e88c39de7f2567a41
minte9 <catalinprescure@gmail.com>
2022-12-27 13:29:36 +0200

    m

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -9,9 +9,9 @@ echo "touch"                ; touch myfile.tmp  ; gsp
 echo "write"                ; echo "A" > myfile.tmp ; gsp
 echo "git commit"           ; gc ; gs
 
-echo "overwrite"            : gsp
-echo "B" > "myfile.tmp"
-echo "C" >> "myfile.tmp" : gsp
-echo "git diff"             ; git diff HEAD^  "myfile.tmp"   | tab2lines
+echo "overwrite"            ; gsp
+echo "B" > myfile.tmp
+echo "C" >> myfile.tmp : gsp
+echo "git diff"             ; git diff HEAD myfile.tmp  | tab2lines
 
-git rm -f "myfile.tmp" > /dev/null
+git rm -f myfile.tmp > /dev/null
