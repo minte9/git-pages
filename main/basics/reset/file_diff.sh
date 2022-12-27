e4648a8642dfd80ba2082ef2cfd15b00935d302d
minte9 <catalinprescure@gmail.com>
2022-12-27 12:44:53 +0200

    My commit

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -8,7 +8,7 @@
 echo -n "1" > "myfile.tmp"
 
 echo "git add"          ; git add .                     ; gsp
-echo "git commit"       ; git commit --oneline -m 'My commit'     | tab2lines
+echo "git commit"       ; git commit --quiet -m 'My commit'
 echo "git last log"     ; git log -n 1 --oneline        | tab2lines 
 
 echo "2" >> "myfile.tmp"
