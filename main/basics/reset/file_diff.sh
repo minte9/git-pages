64b73447db74088e654db62e0cb3df09bde4173a
minte9 <catalinprescure@gmail.com>
2022-12-27 13:16:05 +0200

    output.sh - update repo

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -7,7 +7,7 @@
 
 echo "touch & write"        ; touch myfile.tmp     
 echo "A" > myfile.tmp
-echo "git add"              ; git add . ; gsp
+echo "git add"              ; git add . ; git comit -am 'm'
 
 echo "overwrite"
 echo "B" > "myfile.tmp"
