ced5baf395c76be82c2b04f50b0cb1a90db8a5d4
minte9 <catalinprescure@gmail.com>
2022-12-27 13:14:04 +0200

    output.sh - update repo

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -11,7 +11,7 @@ echo "git add"              ; git add . ; gsp
 
 echo "overwrite"
 echo "B" > "myfile.tmp"
-echo "BC" >> "myfile.tmp"
+echo "C" >> "myfile.tmp"
 echo "git diff"             ; git diff HEAD^  "myfile.tmp"   | tab2lines
 
 git rm -f "myfile.tmp" > /dev/null
