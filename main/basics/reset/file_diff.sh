e32551b855f8456a14e80c49bc5b1420d0130f9b
minte9 <catalinprescure@gmail.com>
2022-12-27 13:08:34 +0200

    output.sh - update repo

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -6,11 +6,11 @@
 . "../../aliases.sh"
 
 echo "touch & write"        ; touch myfile.tmp     
-echo "1" > myfile.tmp       ; gsp
+echo "<title>A</title>" > myfile.tmp       ; gsp
 echo "git add"              ; git add . ; gsp
 
 echo "write again"
-echo "2" > "myfile.tmp"
+echo "<title>B</title>" > "myfile.tmp"
 echo "git diff"             ; git diff HEAD^ --color-words "myfile.tmp"   | tab2lines
                   
 git rm -f "myfile.tmp" > /dev/null
