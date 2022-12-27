5aa0cd53d518a4a94fee58352e61c1ee56377bcc
minte9 <catalinprescure@gmail.com>
2022-12-27 12:47:39 +0200

    My commit

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -11,7 +11,6 @@ echo "git add"          ; git add .                     ; gsp
 echo "git commit"       ; git commit --quiet -m 'My commit'
 
 echo "yyy" >> "myfile.tmp"
-exit 0
 git diff HEAD^ "myfile.tmp"
                   
 git rm -f "myfile.tmp" > /dev/null
