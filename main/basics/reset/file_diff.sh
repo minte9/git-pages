33eea7831b75e67876c2c5dc74d54d9934c97cb4
minte9 <catalinprescure@gmail.com>
2022-12-27 12:53:00 +0200

    output.sh - update repo

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -8,7 +8,7 @@
 echo "abc" > "myfile.tmp"
 
 echo "git add"          ; git add .                     ; gsp
-echo "git commit"       ; git commit --oneline -m 'My commit'     | tab2lines
+echo "git commit"       ; gsp
 
 echo "yyy" >> "myfile.tmp"
 echo "git diff"         ; git diff HEAD^ "myfile.tmp"   | tab2lines
