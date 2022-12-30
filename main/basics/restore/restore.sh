739b0d2ad254447099a8f359d1e01372731bdfb2
minte9 <catalinprescure@gmail.com>
2022-12-30 18:36:01 +0200

    m

---
+++ main/basics/restore/restore.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+: "Remove
+"
+
+. "../../aliases.sh"
+
+echo "touch"                ; touch a.txt                   ; gsp
+echo "git add"              ; git add .                     ; gsp
+echo "git commit"           ; git commit -m 'm'             | tab2lines
+
+echo "git rm"               ; git rm a.txt                  ; gsp
+echo "git restore --staged" ; git restore --staged a.txt    ; gs
+echo "git restore"          ; git restore a.txt             ; gs
+
+git rm -f a.txt > /dev/null
