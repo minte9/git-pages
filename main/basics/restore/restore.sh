2bb244db6ea1510345115fb4ec31ef9f6e735615
minte9 <catalinprescure@gmail.com>
2022-12-30 18:49:49 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -6,7 +6,7 @@
 . "../../aliases.sh"
 
 echo "touch"                ; touch a.txt                   ; gs
-echo "git add"              ; git add                    ; gs
+echo "git add"              ; git add  .                    ; gs
 
 echo "git rm"               ; git rm a.txt                  ; gs
 echo "git restore --staged" ; git restore --staged a.txt    ; gs
@@ -14,4 +14,4 @@ echo "git restore"          ; git restore a.txt             ; gs
 
 echo "git commit"           ; git commit -am 'm'             | tab2lines
 
-git rm -f a.txt
+git rm -f a.txt ; gc 
