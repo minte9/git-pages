69bbf47b379e4c9f8d6e7fa791f4e15a9aea8859
minte9 <catalinprescure@gmail.com>
2022-12-30 18:36:31 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -13,4 +13,5 @@ echo "git rm"               ; git rm a.txt                  ; gsp
 echo "git restore --staged" ; git restore --staged a.txt    ; gs
 echo "git restore"          ; git restore a.txt             ; gs
 
+gc
 git rm -f a.txt > /dev/null
