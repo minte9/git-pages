f74e7f5fdf76b2d3a4b3b215a7deb391ae6af455
minte9 <catalinprescure@gmail.com>
2022-12-30 18:26:18 +0200

    m

--- main/basics/remove/remove.sh
+++ main/basics/remove/remove.sh
@@ -11,4 +11,4 @@ echo "git commit"           ; git commit -m 'm'             | tab2lines
 
 echo "git rm"               ; git rm a.txt                  ; gsp
 echo "git restore --staged" ; git restore --staged a.txt    ; gsp
-echo "git restore"          ; git restore a.txt             ; gsp
+echo "git restore"          ; git restore a.txt             ; gs
