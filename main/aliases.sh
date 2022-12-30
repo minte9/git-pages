5e526c441696cf2afea782e19ea0ce5a86483d8f
minte9 <catalinprescure@gmail.com>
2022-12-30 18:48:39 +0200

    m

--- main/aliases.sh
+++ main/aliases.sh
@@ -1,4 +1,4 @@
 alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
 alias gs="git status | sed 's/^/\t/'; echo"
 alias tab2lines="sed 's/^/\t/' ; echo"
-alias gc="git add . ; git commit -am 'm' --quiet"
+alias gc="git add -u . ; git commit -am 'm' --quiet"
