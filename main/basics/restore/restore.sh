2d4dda9a67058a430d2c5c1a46220fa721289860
minte9 <catalinprescure@gmail.com>
2022-12-30 19:57:11 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -2,18 +2,16 @@
 
 : "Restore
 Helps to unstage or even discard uncommitted local changes
+To undo a commit use git checkout
 "
 
 . "../../aliases.sh"
 
-echo "touch"                ; touch a.txt b.txt                 ; gsp
-echo "git add"              ; git add .                         ; gsp
-echo "git restore a"          ; git restore --staged a.txt        ; gsp
-
-echo "git commit"           ; gc ; gsp
-
-echo "git checkout b"         ; git reset HEAD^1 b.txt ; gc ; gsp  # undo commit
-
+echo "touch"            ; touch X Y                 ; gsp
+echo "git add"          ; git add .                 ; gsp
+echo "git restore a"    ; git restore --staged X    ; gsp
+echo "git commit"       ; gc                        ; gsp
+echo "git checkout b"   ; git reset HEAD^1 Y        ; gc ; gsp  
 
 rm a.txt
 rm b.txt
