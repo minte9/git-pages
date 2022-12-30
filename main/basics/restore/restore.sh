1e7f37c41e0a90d2eb4c674bd04f4ae294b758c8
minte9 <catalinprescure@gmail.com>
2022-12-30 19:29:57 +0200

    output.sh - update repo

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -7,19 +7,6 @@
 
 echo "touch"                ; touch a.txt           ; gsp
 echo "git add"              ; git add .             ; gsp
-echo "git rm -f"            ; git rm -f a.txt       | tab2lines
+echo "git rm --cached"      ; git rm --cached a.txt       | tab2lines
 echo "git status"           ; gs
 
-echo "touch"                ; touch b.txt           ; gs
-echo "git add"              ; git add .             ; gs
-echo "git rm --cached"      ; git rm --cached b.txt | tab2lines : gs
-
-echo "git commit"           ; gc
-echo "git status"           ; gsp
-echo "git restore --staged" ; git restore --staged b.txt | tab2lines | gsp
-
-git rm -f b.txt > /dev/null
-
-#echo "git rm --cached"      ; git rm  --cached a.txt    ; gsp
-#echo "git restore --staged" ; git restore --staged a.txt    ; gsp
-#echo "git restore"          ; git restore a.txt             ; gs
