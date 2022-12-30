6a290ee66f9f651287b0834cdec795cded7917d8
minte9 <catalinprescure@gmail.com>
2022-12-30 17:58:16 +0200

    My commit

--- main/basics/remove/remove.sh
+++ main/basics/remove/remove.sh
@@ -10,7 +10,6 @@ echo "git add"          ; git add .                     ; gsp
 echo "git commit"       ; git commit -m 'My commit'     | tab2lines
 
 echo "rm"               ; rm a.txt                      ; gsp
-echo "git add"          ; git add .                     ; gsp
 echo "checkout"         ; git checkout -- a.txt         ; gsp
                   
 git rm -f a.txt > /dev/null
