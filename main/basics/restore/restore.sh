4a3087e82ffff25b07fde1ecf1850754a37ced33
minte9 <catalinprescure@gmail.com>
2022-12-30 20:20:05 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-: "Restore to unstage
-Reset (or checkout) to undo commits
+: "Restore to unstage / Reset to undo commits
 "
 
 . "../../aliases.sh"
@@ -10,8 +9,7 @@ echo "touch"            ; touch X Y                 ; gsp
 echo "git add"          ; git add .                 ; gsp
 echo "git restore X"    ; git restore --staged X    ; gsp
 echo "git commit"       ; gc                        ; gsp
-#echo "git reset Y"      ; git reset HEAD^ Y         ; gc ; gsp  
-git checkout -- Y ; gc ; gsp  
+echo "git reset Y"      ; git reset HEAD^ Y         ; gc ; gsp  
 
 rm X
 rm Y
