67548f919a9329d9c57dd6a12a61180126c4d4ee
minte9 <catalinprescure@gmail.com>
2022-12-30 19:48:03 +0200

    git-pages update

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -6,8 +6,8 @@ Helps to unstage or even discard uncommitted local changes
 
 . "../../aliases.sh"
 
-echo "touch"                ; touch a.txt           ; gsp
-echo "git add"              ; git add .             ; gsp
-echo "git restore --staged" ; git restore --staged a.txt ; gsp
+echo "touch"                ; touch a.txt                   ; gsp
+echo "git add"              ; git add .                     ; gsp
+echo "git restore"          ; git restore --staged a.txt    ; gsp
 
 rm a.txt
