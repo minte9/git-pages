5e1742b2d1619b4f9607428a0b708088e6596642
minte9 <catalinprescure@gmail.com>
2022-12-30 19:09:21 +0200

    output.sh - update repo

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -10,6 +10,10 @@ echo "git add"              ; git add .             ; gsp
 echo "git rm "              ; git rm -f a.txt       | tab2lines
 echo "git status"           ; gs
 
+echo "touch"                ; touch b.txt           ; gsp
+echo "git add"              ; git add .             ; gsp
+echo "git status"           ; gs
+
 #echo "git rm --cached"      ; git rm  --cached a.txt    ; gsp
 #echo "git restore --staged" ; git restore --staged a.txt    ; gsp
 #echo "git restore"          ; git restore a.txt             ; gs
