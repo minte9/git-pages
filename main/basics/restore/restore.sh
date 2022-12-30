d44e72bcc8b283d8bde4509909dfb022b0c28b7d
minte9 <catalinprescure@gmail.com>
2022-12-30 20:12:19 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -11,10 +11,7 @@ echo "touch"            ; touch X Y                 ; gsp
 echo "git add"          ; git add .                 ; gsp
 echo "git restore a"    ; git restore --staged X    ; gsp
 echo "git commit"       ; gc                        ; gsp
-echo "git rest Y"       ; git reset HEAD^ Y         ; gc ; gsp  
+echo "git reset Y"       ; git reset HEAD^ Y         ; gc ; gsp  
 
-echo "rm X"             ; rm X
-echo "git rm Y"         ; git rm Y                  ; gsp
-echo "git add"          ; git add .                 ; gsp
-echo "git commit"       ; gc                        ; gs
-echo "git checkout Y"   ; git checkout -- Y         ; gs
+rm X
+rm Y
