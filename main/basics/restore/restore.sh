bbea489f8742a69fe6a489f6560d3b80ef38c0b0
minte9 <catalinprescure@gmail.com>
2022-12-30 19:51:05 +0200

    output.sh - update repo

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -8,8 +8,7 @@ Helps to unstage or even discard uncommitted local changes
 
 echo "touch"                ; touch a.txt b.txt                 ; gsp
 echo "git add"              ; git add .                         ; gsp
-echo "git restore"          ; git restore --staged a.txt        ; gsp
-echo "git restore"          ; git restore --staged b.txt        ; gsp
+echo "git restore"          ; git restore --staged a.txt b.txt  ; gsp
 
 rm a.txt
 rm b.txt
