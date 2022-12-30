8521586d8fe44975071648af73cf029b44e9c348
minte9 <catalinprescure@gmail.com>
2022-12-30 18:18:28 +0200

    m

--- main/basics/remove/remove.sh
+++ main/basics/remove/remove.sh
@@ -9,7 +9,7 @@ echo "touch"            ; touch a.txt                   ; gsp
 echo "git add"          ; git add .                     ; gsp
 echo "git commit"       ; git commit -m 'm'             | tab2lines
 
-echo "git rm"           ; git rm a.txt                  ; gs
+echo "git rm"           ; git rm a.txt                  ; gsp
 echo "checkout"         ; git checkout -- a.txt         ; gc ; gsp
 echo "git commit"       ; git commit -am 'm'            | tab2lines
 
