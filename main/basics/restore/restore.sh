68ebbea27ee04ad956ee1dfe480d0549e2fe59f7
minte9 <catalinprescure@gmail.com>
2022-12-30 18:55:16 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -12,6 +12,4 @@ echo "git rm"               ; git rm a.txt                  ; gsp
 echo "git restore --staged" ; git restore --staged a.txt    ; gsp
 echo "git restore"          ; git restore a.txt             ; gs
 
-echo "git commit"           ; git commit -am 'm'             | tab2lines
-
 git rm -f a.txt ; gc ; gs
