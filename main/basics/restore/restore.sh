63f6437ea518ca2352e9f85a4070300559bfc75c
minte9 <catalinprescure@gmail.com>
2022-12-30 19:37:39 +0200

    m

--- main/basics/restore/restore.sh
+++ main/basics/restore/restore.sh
@@ -7,7 +7,7 @@
 
 echo "touch"                ; touch a.txt           ; gsp
 echo "git add"              ; git add .             ; gsp
-echo "git rm --cached"      ; git rm --cached --quiet a.txt | tab2lines | gsp
+echo "git rm --cached"      ; git rm --cached --quiet a.txt | tab2lines | gs
 echo "git restore --staged" ; git restore --staged a.txt | tab2lines | gs
 
 rm a.txt
