d4c573faf3bace645bda927d5e23adcfbe234904
minte9 <catalinprescure@gmail.com>
2023-01-03 16:18:30 +0200

    m

--- main/basics/reset/reset.sh
+++ main/basics/reset/reset.sh
@@ -12,29 +12,23 @@ alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
 alias tab2lines="sed 's/^/\t/'; echo"
 
 touch a.txt
-    echo "touch"
-    gsp
+    echo "touch"; gsp
 git add .
-    echo "git add"
-    gsp
+    echo "git add"; gsp
     echo "git commit" 
 git commit -am 'My commit' | tab2lines
     echo "git last log"
 git log -n 1 --oneline | tab2lines 
 
 git reset --soft HEAD~1
-    echo "Reset soft ~1"
-    gsp
+    echo "Reset soft ~1"; gsp
 git commit -aqm 'm'
-    echo "git commit"
-    gs
+    echo "git commit"; gs
 git reset HEAD~1 | tab2lines
-    echo "Reset ~1"
-    gsp
+    echo "Reset ~1"; gsp
 
 git add .
 git commit -qm 'm'
-    echo "git commit"
-    gs
+    echo "git commit"; gs
 
 git rm -f a.txt > /dev/null
