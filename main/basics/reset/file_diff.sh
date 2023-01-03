cbce8c060dedde130598d597dff7559d9e0264c7
minte9 <catalinprescure@gmail.com>
2023-01-03 16:46:36 +0200

    m

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -9,25 +9,25 @@ HEAD^   / refers to the commit before the current commit
 . "../../aliases.sh"
 
 touch myfile
-    echo "touch"
-    gsp
+    echo "touch"; gsp
 
 echo "A" > myfile.tmp   
-    echo "write A"
-    gsp 
+    echo "write A"; gsp 
 
 git add .
 git commit -qm 'm'
-    echo "git commit"
-    gs
-    
-echo "overwrite"            ; echo "B" > myfile.tmp     ; gsp
-echo "write"                ; echo "C" >> myfile.tmp    ; gsp
-echo "git diff"             ; git diff HEAD myfile.tmp  | tab2lines
+    echo "git commit"; gs
+
+echo "B" > myfile.tmp 
+    echo "overwrite"; gsp
+echo "C" >> myfile.tmp 
+    echo "write"; gsp
+
+    echo "git diff"
+git diff HEAD myfile.tmp  | tab2lines
 
 git add .
 git commit -qm 'm'
-    echo "git commit"
-    gs
+    echo "git commit"; gs
 
 git rm -f myfile.tmp > /dev/null
