67c95f8e3027cc53e66299bc1d2bbc6cd1abadcc
minte9 <catalinprescure@gmail.com>
2023-01-03 12:22:52 +0200

    output.sh - update repo

--- main/basics/remove/remove.sh
+++ main/basics/remove/remove.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
 : "Removing files from repository
-
 If a file is not tracked rm file will generate error
 To remove a file and keep it in working directory, use --cached
 To remove a file from working directory, when used with -f
 "
 
-. "../../aliases.sh"
+alias gs="git status | sed 's/^/\t/' ; echo"
+alias gsp="git status --porcelain | sed 's/^/\t/' ; echo"
 
-touch a.tmp                         ; echo 'touch'           ; gsp
-git add a.tmp                       ; echo 'git add'
-git rm --cached a.tmp > /dev/null   ; echo 'git rm --cached' ; gsp 
-git add a.tmp                       ; echo 'git add' 
-git rm -f a.tmp > /dev/null         ; echo 'git rm -f'       ; gs
+touch a.tmp                         
+    echo 'touch'
+    gsp
+
+git add a.tmp
+    echo 'git add'
+    gsp
+
+git rm -q --cached a.tmp
+    echo 'git rm --cached'
+    gsp
+
+git add a.tmp
+    echo 'git add'
+    gsp
+
+git rm -fq a.tmp
+    echo 'git rm -f'
+    gs
