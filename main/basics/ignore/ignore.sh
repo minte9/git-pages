6fc9558d8e5edf4dde1ca17515f4ba5f5c332895
minte9 <catalinprescure@gmail.com>
2023-01-03 12:17:03 +0200

    m

--- main/basics/ignore/ignore.sh
+++ main/basics/ignore/ignore.sh
@@ -8,27 +8,27 @@ alias gs="git status | sed 's/^/\t/' ; echo"
 alias gsp="git status --porcelain | sed 's/^/\t/' ; echo"
 
 touch a.txt b.tmp
-echo "touch a b"
+    echo "touch a b"
 gsp
 
 echo "*.tmp" > .gitignore
-echo "*.tmp > .gitignore"
+    echo "*.tmp > .gitignore"
 gsp
 
 git add .
-echo "git add ."
+    echo "git add ."
 gsp
 
 git commit -qm 'm'
-echo "git commit"
+    echo "git commit"
 gs
 
 git rm -q .gitignore
 git rm -q a.txt
 rm b.tmp 
-echo "git rm a .gitignore / rm b"
+    echo "git rm a .gitignore / rm b"
 gsp
 
 git commit -aqm 'm'
-echo "git commit -a"
+    echo "git commit -a"
 gs
