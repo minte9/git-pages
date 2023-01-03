2de2d8e875c35c099f93abca4b598adbfacd5c71
minte9 <catalinprescure@gmail.com>
2023-01-03 16:50:50 +0200

    m

--- main/basics/reset/file_diff.sh
+++ main/basics/reset/file_diff.sh
@@ -6,7 +6,9 @@ HEAD    / refers to current commit
 HEAD^   / refers to the commit before the current commit
 "
 
-. "../../aliases.sh"
+alias gs="git status | sed 's/^/\t/'"
+alias gsp="git status --porcelain | sed 's/^/\t/'; echo"
+alias tab2lines="sed 's/^/\t/'; echo"
 
 touch myfile
     echo "touch"; gsp
